b3fd4f9a2c9f86ecbfeab5068f9826fa4a3b828e
zheyuan xie <zyxie.sjtu@gmail.com>
2018-08-15 20:13:14 +0800

    Add shortcut scripts

---
+++ rmsc/misc/shell/RED_WING.sh
@@ -0,0 +1,17 @@
+# Seer Node
+cp /home/dji/catkin_ws/src/rmsc4/rmsc/seer/proto/arguments.red.prototxt /home/dji/catkin_ws/src/rmsc4/rmsc/seer/proto/arguments.prototxt
+
+# Decision Node
+cp /home/dji/catkin_ws/src/rmsc4/rmsc/decision_v1/config/decision.wing.prototxt /home/dji/catkin_ws/src/rmsc4/rmsc/decision_v1/config/decision.prototxt
+
+# Armor Detection Node
+cp /home/dji/catkin_ws/src/rmsc4/modules/perception/detection/armor_detection/constraint_set/config/constraint_set.red.prototxt /home/dji/catkin_ws/src/rmsc4/modules/perception/detection/armor_detection/constraint_set/config/constraint_set.prototxt
+
+# Color Detection Node
+cp /home/dji/catkin_ws/src/rmsc4/modules/perception/detection/color_detection/config/color_detection.red.prototxt /home/dji/catkin_ws/src/rmsc4/modules/perception/detection/color_detection/config/color_detection.prototxt
+
+# Ammo Visual Detection Node
+cp /home/dji/catkin_ws/src/rmsc4/rmsc/ammo_visual_detect/proto/arguments.wing.red.prototxt /home/dji/catkin_ws/src/rmsc4/rmsc/ammo_visual_detect/proto/arguments.prototxt
+
+# Localization Node
+cp /home/dji/catkin_ws/src/rmsc4/modules/perception/localization/config/localization.wing.red.prototxt /home/dji/catkin_ws/src/rmsc4/modules/perception/localization/config/localization.prototxt
